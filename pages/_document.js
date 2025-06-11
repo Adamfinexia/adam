818ad7e660d17dc4ceae2569a1f442e669a30d04
Adamfinexia <adam.finexia@icloud.com>
2025-06-11 23:37:08 +0200

    Add files via upload

--- pages/_document.js
+++ pages/_document.js
@@ -3,10 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="pl">
       <Head>
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#317EFB"/>
+        <meta name="theme-color" content="#317EFB" />
       </Head>
       <body>
         <Main />
