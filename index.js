2102f1b891c78889e31d3bfe4c84d317c9d63c1a
Adamfinexia <adam.finexia@icloud.com>
2025-06-12 00:27:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
 
-export default function Home() {
-  return (
-    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
-      <h1>Finexia działa!</h1>
-      <p>To Twoja aplikacja budowlana online.</p>
-    </div>
-  );
-}
