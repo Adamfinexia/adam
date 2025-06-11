0ee34dbf893232bc5937a32ce11af9f9fa7c5793
Adamfinexia <adam.finexia@icloud.com>
2025-06-11 22:33:42 +0200

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
+      <h1>Finexia działa!</h1>
+      <p>To Twoja aplikacja budowlana online.</p>
+    </div>
+  );
+}
