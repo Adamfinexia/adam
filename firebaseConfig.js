bcd0ff1180d16441f7458ca2de92ea5d31d57ab1
Adamfinexia <adam.finexia@icloud.com>
2025-06-11 18:13:02 +0200

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,18 +1,8 @@
-
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCjvaFRK1AHb7un99VZchZj1Mjih3PSCcs",
-  authDomain: "finexia-bv.firebaseapp.com",
-  projectId: "finexia-bv",
-  storageBucket: "finexia-bv.firebasestorage.app",
-  messagingSenderId: "963249060796",
-  appId: "1:963249060796:web:c1414d058b636fcd5b49b9",
-  measurementId: "G-NGSCTG1HXX"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
