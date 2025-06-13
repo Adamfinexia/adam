513648f05cca044be690b1fcddc9f7f6762fe7cf
Adamfinexia <adam.finexia@icloud.com>
2025-06-13 09:46:04 +0200

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { signInWithEmailAndPassword } from '../firebaseConfig';
+import { signInWithEmailAndPassword } from './firebaseConfig';
 import { auth } from '../firebaseConfig';
 import { useRouter } from 'next/router';
 
